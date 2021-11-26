837c7b094ea5fcf21180f563a4b2905413ca11aa
pauloricardolongi <pauloricardolongi@gmail.com>
2021-11-26 03:18:59 -0300

    Programa: thows parseException

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,19 +1,23 @@
 package aplicacao;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import entidade.Comentario;
 import entidade.Publicar;
 
 public class Programa {
-	    public static void main(String[]args) {
+	    public static void main(String[]args) throws ParseException {
 	    	
 	    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 	    
 		Comentario c1 = new Comentario("Have a nice trip!");
 		Comentario c2 = new Comentario("Wow that s awesome");
 		
-		Publicar p1 = new Publicar(momento, titulo,content,likes);
+		Publicar p1 = new Publicar(sdf.parse("21/06/2018 13:05:44"),
+				"Traveling to New Zealend",
+				"Vou visitar esse país",
+				12);
 	}
 
 }
