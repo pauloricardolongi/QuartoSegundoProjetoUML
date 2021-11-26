e3a0cfb541850e63ca93c112fadfb5c4831628c1
pauloricardolongi <pauloricardolongi@gmail.com>
2021-11-26 02:25:30 -0300

    Programa: criar SimpleDateFormat por causa do atributo momento

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,15 +1,19 @@
 package aplicacao;
 
+import java.text.SimpleDateFormat;
+
 import entidade.Comentario;
 import entidade.Publicar;
 
 public class Programa {
-
-	public static void main(String[] args) {
+	    public static void main(String[]args) {
+	    	
+	    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	    
 		Comentario c1 = new Comentario("Have a nice trip!");
 		Comentario c2 = new Comentario("Wow that s awesome");
 		
-		Publicar p1 = new Publicar(null, null, null, null)
+		Publicar p1 = new Publicar(momento, titulo,content,likes);
 	}
 
 }
